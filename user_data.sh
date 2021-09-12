28a0540685d8f9888fc3445c79d5e3f1fbbda8d1
Nicholas Pym <nicholasp@bbd.co.za>
2021-09-12 13:10:32 +0200

    Edited user data script

--- user_data.sh
+++ user_data.sh
@@ -2,7 +2,10 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+source .bashrc
 nvm install node
+sudo yum update -y
+sudo yum install git -y
 git clone https://github.com/NicPym/game-jam-server.git
 cd game-jam-server
 npm install
