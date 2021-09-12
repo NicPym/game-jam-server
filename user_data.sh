511e4731879e6897807ab85dd160180a40cd5edc
Nicholas Pym <nicholasp@bbd.co.za>
2021-09-12 15:15:10 +0200

    Updated user data

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-source .bashrc
-nvm install node
 sudo yum update -y
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
 sudo yum install git -y
 git clone https://github.com/NicPym/game-jam-server.git
 cd game-jam-server
